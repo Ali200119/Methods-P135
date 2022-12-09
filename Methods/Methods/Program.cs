1ad7c1c3665df1edd876c2c81e4dac0d09e69359
Ali200119 <aliit@code.edu.az>
2022-12-09 14:45:26 +0400

    Update

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,117 @@
+﻿#region Task 4
+
+//int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+
+//Console.WriteLine(Sum(array));
+
+//static int Sum(int[] array)
+//{
+//    int sum = 0;
+
+//    foreach (var item in array)
+//    {
+//        if (item % 2 == 1)
+//        {
+//            sum += item;
+//        }
+//    }
+
+//    return sum;
+//}
+
+#endregion
+
+#region Task 5
+
+//int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+
+//Console.WriteLine(Count(array));
+
+//static int Count(int[] array)
+//{
+//    int count = 0;
+
+//    foreach (var item in array)
+//    {
+//        if (item % 2 == 0)
+//        {
+//            count ++;
+//        }
+//    }
+
+//    return count;
+//}
+
+#endregion
+
+#region Task 6
+
+//int n = int.Parse(Console.ReadLine());
+//int m = int.Parse(Console.ReadLine());
+
+//Console.WriteLine(Count(n, m));
+
+//static int Count(int n, int m)
+//{
+//    int count = 0;
+
+//	for (int i = n; i < m; i++)
+//	{
+//		if (i % 2 == 1)
+//		{
+//			count++;
+//		}
+//	}
+
+//	return count;
+//}
+
+#endregion
+
+#region Task 7
+
+//int n = int.Parse(Console.ReadLine());
+//int m = int.Parse(Console.ReadLine());
+
+//Console.WriteLine(Sum(n, m));
+
+//static int Sum(int n, int m)
+//{
+//    int sum = 0;
+
+//	for (int i = n; i < m; i++)
+//	{
+//		if (i % 2 == 0)
+//		{
+//			sum += i;
+//		}
+//	}
+
+//	return sum;
+//}
+
+#endregion
+
+#region Task 8
+
+//int n = int.Parse(Console.ReadLine());
+//int m = int.Parse(Console.ReadLine());
+
+//Console.WriteLine(Count(n, m));
+
+//static int Count(int n, int m)
+//{
+//	int count = 0;
+
+//	for (int i = n; i < m; i++)
+//	{
+//		if (i % 2 == 0)
+//		{
+//			count++;
+//		}
+//	}
+
+//	return count;
+//}
+
+#endregion
